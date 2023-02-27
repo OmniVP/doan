f51d2e5e3c19eb4c969be97924adcf2c4ef56706
Nguyen Nhut <nguyen.nhut.99.2017@gmail.com>
2023-02-27 10:39:33 +0700

    update

--- server/server.js
+++ server/server.js
@@ -3,11 +3,10 @@ const cors = require("cors");
 
 const app = express();
 
-var corsOptions = {
-  origin: "http://localhost:8081"
-};
-
-app.use(cors(corsOptions));
+app.use(cors())
+ app.listen(80, function () {
+  console.log('CORS-enabled web server listening on port 80')
+})
 
 // parse requests of content-type - application/json
 app.use(express.json());
